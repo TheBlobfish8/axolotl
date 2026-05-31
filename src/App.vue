0c8eba2cb5f4f0cd1c2e871acd0c77f9eb5699a4
TheBlobfish8 <Thebestandgreatest040404@gmail.com>
2026-05-31 02:01:51 -0600

    start navbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+	import { FwbNavbar, FwbNavbarCollapse, FwbNavbarLink } from "flowbite-vue";
+</script>
 
 <template>
-	<nav>
-		<RouterLink to="/">Overview</RouterLink>
-		<RouterLink to="/about">About</RouterLink>
-	</nav>
+	<fwb-navbar>
+		<template #default="{ isShowMenu }">
+			<fwb-navbar-collapse :is-show-menu="isShowMenu">
+				<fwb-navbar-link>
+					<RouterLink to="/">Home</RouterLink>
+				</fwb-navbar-link>
+				<fwb-navbar-link>
+					<RouterLink to="/servers">Servers</RouterLink>
+				</fwb-navbar-link>
+				<fwb-navbar-link>
+					<RouterLink to="/about">About</RouterLink>
+				</fwb-navbar-link>
+			</fwb-navbar-collapse>
+		</template>
+	</fwb-navbar>
 	<main>
 		<RouterView />
 	</main>
