39a82e267367858f45c6c14ca130f194223bf94a
TheBlobfish8 <Thebestandgreatest040404@gmail.com>
2026-05-30 18:57:48 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup></script>
+
+<template>
+	<nav>
+		<RouterLink to="/">Overview</RouterLink>
+		<RouterLink to="/about">About</RouterLink>
+	</nav>
+	<main>
+		<RouterView />
+	</main>
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+	import { ref } from "vue";
+
+	const count = ref(0);
+</script>
+
+<template>
+	<h1>Home</h1>
+	<p>Count: {{ count }}</p>
+	<button @click="count++">Increase Count</button>
+</template>
+
+<style scoped></style>
